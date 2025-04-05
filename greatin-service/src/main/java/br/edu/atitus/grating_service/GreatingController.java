94cdf5b5142eaff920633e96b05dc70723ac1f07
ViniZERAAAA <viniciuscasturino1551@gmail.com>
2025-04-04 22:11:44 -0300

    auala 04/04

--- greatin-service/src/main/java/br/edu/atitus/grating_service/GreatingController.java
+++ greatin-service/src/main/java/br/edu/atitus/grating_service/GreatingController.java
@@ -1,16 +1,32 @@
 package br.edu.atitus.grating_service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("greatin-service")
 public class GreatingController {
+	@Value("${greating-service.greating}")
+	private String greating;
+	@Value("${greating-service.default-name}")
+	private String defaultName;
 	
-	@GetMapping
-	public String getGreatin() {
-		return  ("Hello World!");
+	
+	@GetMapping({"","/","/{namePath}"})
+	public ResponseEntity<String> getGreatin(
+			@PathVariable(required = false) String namePath,
+			@RequestParam(required = false) String name 
+			) {
+		if (name == null)
+			name = namePath != null ? namePath : defaultName;
+		
+		String returnText = String.format("%s %s!",greating, name);
+		return ResponseEntity.ok(returnText);
 	}
 
 }
